13bb396a9feb9240528fcc4fbe4ba42cd3f5db8e
Ben McEvoy <ben.mcevoy@gmail.com>
2015-10-12 20:49:29 +1100

    upgrade all the things

--- Singleton/Singleton/Program.cs
+++ Singleton/Singleton/Program.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.Practices.Unity;
 using Radio7.Unity.Decorators;
 
